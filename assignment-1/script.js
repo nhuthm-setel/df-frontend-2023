29fc8e17dc9dbea18d6e255bd1ca7560e134535f
Nhut Huynh <nhuthuynh.steve@setel.com>
2023-09-16 21:45:55 +0700

    chore: update assignment 1 - Implemented search feature - Enhanced with the create new item modal

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -4,8 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const itemDescriptionInput = document.getElementById("itemDescription");
     const itemTopicInput = document.getElementById("itemTopic");
     const itemList = document.getElementById("itemList");
+    const searchInput = document.getElementById("searchInput");
 
     addItemBtn.addEventListener("click", function() {
+        const createNewItemModal = document.getElementById("createItemModal");
+        createNewItemModal.style.display = "block";
+    });    
+
+    const createNewItemBtn = document.getElementById("createNewItemBtn");
+    createNewItemBtn.addEventListener("click", function () {
         const itemName = itemNameInput.value;
         const itemDescription = itemDescriptionInput.value;
         const itemTopic = itemTopicInput.value;
@@ -44,4 +51,35 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     });
+
+    const closeButtons = document.querySelectorAll(".close");
+    closeButtons.forEach(function (closeButton) {
+        closeButton.addEventListener("click", function() {
+            const modal = closeButton.closest(".modal");
+            if (modal) {
+                modal.style.display = "none";
+            }
+        });
+    });
+
+    function filterItems() {
+        const searchText = searchInput.value.toLowerCase();
+        const rows = itemList.querySelectorAll("tr");
+
+        rows.forEach(function (row) {
+            const itemNameCell = row.querySelector("td:nth-child(1)");
+            const itemDescriptionCell = row.querySelector("td:nth-child(2)");
+
+            if (itemNameCell && itemDescriptionCell) {
+                const itemName = itemNameCell.textContent.toLowerCase();
+                const itemDescription = itemDescriptionCell.textContent.toLowerCase();
+                if (itemName.includes(searchText) || itemDescription.includes(searchText)) {
+                    row.style.display = ""; // Show the row
+                } else {
+                    row.style.display = "none"; // Hide the row
+                }
+            }
+        });
+    }
+    searchInput.addEventListener("input", filterItems);
 });
